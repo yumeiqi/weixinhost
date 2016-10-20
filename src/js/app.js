a7f333c8f808e5ee45f060050a05259656b1fe2d
于美琪 <986823092@qq.com>
2016-10-20 18:11:49 +0800

    增加页面跳转

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,59 @@
-var app = angular.module('weixinhostApp',[]);
-app.controller("title",function($scope){
+var app = angular.module('weixinhostApp',['ui.router']);
+
+// // /*controller*/
+// app.controller("termCtrl",function($scope){
+// })
+
+// // .controller("IndexCtrl",function($scope){
+// // })
+
+// app.controller("aboutCtrl",function($scope){
+// })
+
+// // .controller("PartnersCtrl",function($scope){
+// // })
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+    .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'tab.html',
+        // controller: "aboutCtrl"
+    })
+
+    .state('tab.index', {
+        url: '/index',
+        views: {
+            '': {
+                templateUrl: 'index.html',
+                // controller: 'ProductListCtrl',
+            }
+        },
+    })
+
+    // .state('tab.index', {
+    //     url: '/index',
+    //     templateUrl: 'index.html',
+    //     // controller: "aboutCtrl"
+    // })
+
+    .state('tab.about', {
+        url: '/about',
+        templateUrl: 'about.html',
+        // controller: "termCtrl"
+    })
+
+    .state('tab.term', {
+        url: '/term',
+        templateUrl: 'term.html',
+        // controller: "termCtrl"
+    })
+
+    $urlRouterProvider.otherwise('tab/index');
 
 
-});
+})
+
